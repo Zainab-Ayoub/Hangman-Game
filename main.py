01232c98ca8cc1b1c99b745b615320007c12b649
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-18 17:20:20 +0500

    improved UI

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
     for position in range(word_length):
         letter = chosen_word[position]
         print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
@@ -26,6 +29,7 @@ while not end_of_game:
             display[position] = letter
 
     if guess not in chosen_word:
+        print(f"You've guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
             end_of_game = True
