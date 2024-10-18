0a3d8ff3225aa61a13eceded4f19460d5f6cc72c
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-18 17:11:23 +0500

    logic completed

--- main.py
+++ main.py
@@ -1,26 +1,94 @@
 import random
-word_list = ["ardvark", "baboon", "camel"]
 
-chosen_word = random.choice(word_list)
-print(f"The solution is {chosen_word}")
-length = len(chosen_word)
-
-display = []
-
-for _ in range (0, length):
-    display += "_"
-print(display)
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+
+lives = 6
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in range(word_length):
+    display += "_"
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    for position in range (0, length):
+
+    for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
-            display[position] = letter
-print(display)       
+            display[position] = letter 
+ 
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
 
-if "_" in display:
-    end_of_game = True
-    print("You win!")
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+   
+    print (stages[lives])
